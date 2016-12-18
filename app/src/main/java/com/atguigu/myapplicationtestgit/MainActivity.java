8fe51c98fffa6e1f2a42b9f8db0c4629ae15012a
KiddyBoy <569022914@qq.com>
2016-12-18 20:03:04 +0800

    测试2

--- app/src/main/java/com/atguigu/myapplicationtestgit/MainActivity.java
+++ app/src/main/java/com/atguigu/myapplicationtestgit/MainActivity.java
@@ -2,12 +2,17 @@ package com.atguigu.myapplicationtestgit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
+/**
+ * 练习使用Git
+ */
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("TAG", "啦啦啦");
     }
 }
